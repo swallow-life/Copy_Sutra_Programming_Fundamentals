097092c0e0bf3738621ed5d85d70f60d11329e6e
swallow-life <mshk_stt@yahoo.co.jp>
2016-04-14 03:12:58 +0000

    modify ex8-3, add ketsuekigata

--- c8/ex8-3.ml
+++ c8/ex8-3.ml
@@ -2,6 +2,7 @@ type person_t = {
 	namae : string;
 	shinchou_m : float;
 	taijyuu_kg : float;
-	tanjyoubi_tsukihi : (int * int)
+	tanjyoubi_tsukihi : (int * int);
+	ketsuekigata : string;
 }
-let data1 = {namae="John Doe"; shinchou_m=1.72; taijyuu_kg=60.2; tanjyoubi_tsukihi=(4,11)}
+let data1 = {namae="John Doe"; shinchou_m=1.72; taijyuu_kg=60.2; tanjyoubi_tsukihi=(4,11); ketsuekigata="O"}
