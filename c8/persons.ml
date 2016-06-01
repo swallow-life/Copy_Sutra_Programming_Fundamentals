5c7a7b5ce082e1f7bf16d0d986d615b550c1696c
swallow-life <mshk_stt@yahoo.co.jp>
2016-06-01 03:33:15 +0000

    examples of type person_t

---
+++ c8/persons.ml
@@ -0,0 +1,23 @@
+let person1 =
+  {namae = "浅井";
+   shinchou_m = 1.72;
+   taijyuu_kg = 58.5;
+   tanjyoubi_tsukihi = (9,17);
+   ketsuekigata = "A"
+}
+
+let person2 = {
+  namae = "宮原";
+  shinchou_m = 1.63;
+  taijyuu_kg = 55.0;
+  tanjyoubi_tsukihi = (6,30);
+  ketsuekigata = "B"
+}
+
+let person3 = {
+  namae = "中村";
+  shinchou_m = 1.68;
+  taijyuu_kg = 63.0;
+  tanjyoubi_tsukihi = (6,6);
+  ketsuekigata = "O"
+}
