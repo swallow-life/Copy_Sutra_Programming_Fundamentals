fd1c69250cd2dde77efb8def93d9cf68784a7a31
swallow-life <mshk_stt@yahoo.co.jp>
2016-08-05 03:17:28 +0000

    add sig to tree module.

--- c19/tree.ml
+++ c19/tree.ml
@@ -1,5 +1,20 @@
 module Tree : sig
+	type ('a, 'b) t
+	(* キーが'a型,値が'b型の木の型。型の中身は非公開 *)
 
+	val empty : ('a, 'b) t
+(* 	使い方：empty *)
+(* 	空の木 *)
+
+	val insert : ('a, 'b) t -> 'a -> 'b -> ('a, 'b) t
+(* 	使い方：insert tree key value *)
+(* 木treeにキーkeyと値valueを挿入した木を返す *)
+(* キーが既に存在していたら新しい値に置き換える *)
+
+	val search : ('a, 'b) t -> 'a -> 'b
+(* 	使い方：search tree key *)
+(* 	木treeの中からキーkeyに対応する値を探して返す *)
+(* 	みつからなければNot_foundをraiseする *)
 end = struct
 	(* 2分探索木を表す型 *)
 	type ('a, 'b) t = Empty
